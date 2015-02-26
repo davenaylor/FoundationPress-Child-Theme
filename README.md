f152caeac6f5a3655f8d421b12424f4bb3d95425
Dave Naylor <dave@caramboo.com>
2015-02-26 17:28:08 +0000

    Reflect changes to upstream parent theme

--- README.md
+++ README.md
@@ -8,12 +8,17 @@ This child theme uses its own Foundation installation.  All javascript, sass
 files and bower packages are self contained and don't need to be referenced from
 the parent theme. It's essentially a mirror of the FoundationPress structure. 
 
-**You'll need to have the following items installed before continuing.**
+**Okay, so you'll need to have the following items installed before continuing.**
 
-  * [Node.js](http://nodejs.org): Use the installer provided on the NodeJS
-  website.
+  * [Node.js](http://nodejs.org): Due to an unresolved incompability issue with node-sass, we will need to use Node v. 0.10.33 (stable). [See this issue for a more detailed description](https://github.com/zurb/foundation-libsass-template/issues/16#issuecomment-76017152)
+    * [Mac installer](https://nodejs.org/dist/v0.10.33/node-v0.10.33.pkg)
+    * [Windows installer](https://nodejs.org/dist/v0.10.33/node-v0.10.33-x86.msi)
+    * [Windows x64 installer](https://nodejs.org/dist/v0.10.33/x64/node-v0.10.33-x64.msi)
+    * [Other release files](https://nodejs.org/dist/v0.10.33/)
+    * [Documentation](https://nodejs.org/docs/v0.10.33/api/)
   * [Grunt](http://gruntjs.com/): Run `[sudo] npm install -g grunt-cli`
   * [Bower](http://bower.io): Run `[sudo] npm install -g bower`
+
 
 ## Quickstart
 
@@ -24,7 +29,7 @@ git clone git@github.com:davenaylor/FoundationPress-Child-Theme your-theme-name
 cd your-theme-name
 npm install && bower install && grunt build
 ```
-You don't need the bower/node installation on the parent theme since we've installed
+You don't need the bower/node installation in the parent theme since we've installed
 all the required components in the child.
 
 **Tip 1:** 
@@ -43,3 +48,4 @@ While you're working on your project, run:
 `grunt watch`
 
 And you're set!
+
--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -2,49 +2,31 @@
 
 function FoundationPress_scripts() {
 
-  // deregister the jquery version bundled with wordpress
-  wp_deregister_script( 'jquery' );
+    // Deregister the jquery version bundled with wordpress
+    wp_deregister_script( 'jquery' );
 
-  // enqueue modernizr, jquery and foundation
-  wp_enqueue_script( 'modernizr', get_stylesheet_directory_uri() . '/js/vendor/modernizr.js', array(), '1.0.0', false );
-  wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/vendor/jquery.js', array(), '1.0.0', false );
-  wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/js/foundation.js', array('jquery'), '1.0.0', true );
+    // Modernizr is used for polyfills and feature detection. Must be placed in header. (Not required)
+    wp_register_script( 'modernizr', get_stylesheet_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false );
 
-  // enqueue scripts
-  wp_enqueue_script('modernizr');
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('foundation');
+    // Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header. (Not required)
+    wp_register_script( 'fastclick', get_stylesheet_directory_uri() . '/js/vendor/fastclick.js', array(), '1.0.0', false );
 
-  //if(is_home() || is_front_page()):
-      //wp_register_script( 'foundation.slider', get_template_directory_uri() . '/js/foundation/js/foundation/foundation.slider.js', array('jquery'), '1.0.0', true );
-      //wp_enqueue_script('foundation.slider');
-  //endif;
+    // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+    wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
-}
+    // Self hosted jQuery placed in the footer. (Comment the script above and uncomment the script below if you want to switch).
+    //wp_register_script( 'jquery', get_stylesheet_directory_uri() . '/js/vendor/jquery.js', array(), '2.1.3', true );
+
+    // If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88
+    // It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
+    wp_register_script( 'foundation', get_stylesheet_directory_uri() . '/js/foundation.js', array('jquery'), '5.5.1', true );
+
+    // Enqueue all registered scripts
+    wp_enqueue_script('modernizr');
+    wp_enqueue_script('fastclick');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('foundation');
+
+  }
+
 add_action( 'wp_enqueue_scripts', 'FoundationPress_scripts' );
-
-
-function FoundationPress_js_script() {
-?>
-<script type="text/javascript">
-
-if ( undefined !== window.jQuery ) {
-
-  jQuery( document ).ready(function( $ ) {
-
-		(function( $ ) {
-
-			$(document).foundation();
-
-		})(jQuery);
-
-	})
-}
-
-</script>
-<?php
-}
-add_action( 'wp_footer', 'FoundationPress_js_script',10);
-
-?>
-
