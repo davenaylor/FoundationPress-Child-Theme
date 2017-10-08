0c50dca9f0003ec92af7042e487fec101a21ab63
Dave Naylor <dave@caramboo.com>
2017-10-08 15:32:14 +0100

    Updating changes in parent theme

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -1,27 +1,39 @@
 <?php
+/**
+ * Enqueue all styles and scripts
+ *
+ * Learn more about enqueue_script: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_script}
+ * Learn more about enqueue_style: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_style }
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
 
-function foundationpress_scripts() {
+if ( ! function_exists( 'foundationpress_scripts' ) ) :
+	function foundationpress_scripts() {
 
-    // Enqueue the main Stylesheet.
-    wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.3.0', 'all' );
+	// Enqueue the main Stylesheet.
+	wp_enqueue_style( 'main-stylesheet', get_template_directory_uri() . '/dist/assets/css/app.css', array(), '2.10.3', 'all' );
 
-    // Deregister the jquery version bundled with WordPress.
-    wp_deregister_script( 'jquery' );
+	// Deregister the jquery version bundled with WordPress.
+	wp_deregister_script( 'jquery' );
 
-    // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-    wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', false );
 
-    // If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88.
-    // It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-    wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.3.0', true );
+	// Enqueue Founation scripts
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/dist/assets/js/app.js', array( 'jquery' ), '2.10.3', true );
 
-    // Add the comment-reply library on pages where it is necessary
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
+	// Enqueue FontAwesome from CDN. Uncomment the line below if you don't need FontAwesome.
+	//wp_enqueue_script( 'fontawesome', 'https://use.fontawesome.com/5016a31c8c.js', array(), '4.7.0', true );
 
-}
 
-add_action( 'wp_enqueue_scripts', 'foundationpress_scripts' );
+	// Add the comment-reply library on pages where it is necessary
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 
-?>
+	}
+
+	add_action( 'wp_enqueue_scripts', 'foundationpress_scripts' );
+endif;
